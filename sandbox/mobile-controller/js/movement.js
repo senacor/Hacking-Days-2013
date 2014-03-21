d9a7b87ff87e3f3ed2e818505471df161a7c0035
Svetoslav <sceak@abv.bg>
2014-03-21 18:05:28 +0100

    adding initial sprite support

--- sandbox/mobile-controller/js/movement.js
+++ sandbox/mobile-controller/js/movement.js
@@ -19,7 +19,8 @@ function init() {
     window.addEventListener("keydown", handlePressedKey, false);
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext('2d');
-    timer=setInterval(draw, 10);
+    timer=setInterval(draw, 200);
+    img.sprite = createSprite(4, [0], false);
     return timer;
 }
 
@@ -27,24 +28,40 @@ function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#773333";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, x, y, width, height);
+    img.sprite.update(1 );
+    img.sprite.canvasPos = [x, y];
+    img.sprite.render(ctx);
 }
 
 function handlePressedKey(event) {
     //left arrow
     if (event.keyCode == 37 && x > 10) {
         x-=20;
+        img.sprite = createSprite(4, [8, 9, 8, 7], true)
     }
     //up arrow
     else if (event.keyCode == 38 && y > 10) {
         y-=20;
+        img.sprite = createSprite(5, [0, 1, 2], true)
+
     }
     //right_arrow
     else if (event.keyCode == 39 && x < 441) {
         x+=20;
+        img.sprite = createSprite(5, [7, 8, 9], true)
+
     }
     //down_arrow
     else if (event.keyCode == 40 && y < 421) {
         y+=20;
+        img.sprite = createSprite(4, [0, 1, 2], true)
     }
+}
+
+function createSprite(row, frames, playOnce){
+     return new Sprite("img/bomber.png", [0, calcRow(row)], [26, 29], 1, frames, "horizontal", playOnce);
+}
+
+function calcRow(r){
+    return r*29;
 }
