35e09eeef68728e33ef91971aada28001f0c1ec5
Andri Bremm <code@andribremm.de>
2014-03-21 21:11:27 +0100

    Ein Spielfeld setzt sich aus vielen Feldern zusammen.

---
+++ bomberman-server/src/main/java/com/senacor/bm/model/Feldart.java
@@ -0,0 +1,14 @@
+package com.senacor.bm.model;
+
+/**
+ * Created by abremm on 21.03.14.
+ */
+public enum Feldart {
+
+    LEER,
+    WAND,
+    STEIN,
+    PORTAL;
+
+
+}
--- bomberman-server/src/main/java/com/senacor/bm/model/Spielfeld.java
+++ bomberman-server/src/main/java/com/senacor/bm/model/Spielfeld.java
@@ -1,5 +1,45 @@
 package com.senacor.bm.model;
 
 public class Spielfeld {
-    
+
+    private int width = 10;
+    private int height = 10;
+    private Feldart[][] felder;
+
+    public Spielfeld(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.felder = new Feldart[width][height];
+    }
+
+    /**
+     *
+     * @param x verticale Position.
+     * @param y horizontale Position.
+     * @param feldart Die Art des Feldes.
+     */
+    public void setFeld(int x, int y, Feldart feldart) {
+        felder[x][y] = feldart;
+    }
+
+    /**
+     * @param x verticale Position.
+     * @param y horizontale Position.
+     * @return Die Art des Feldes.
+     */
+    public Feldart getFeld(int x, int y) {
+        return felder[x][y];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
+
+
+
+
