11a307182de6ec451fd1abf1f066a4d5fce804d7
jochenchrist <jchrist@chrjo-ambp.intern>
2014-03-22 12:29:11 +0100

    Match Maker Verticle added

---
+++ bomberman2-matchmaker/README.md
@@ -0,0 +1,7 @@
+In vertx/sys-mods
+
+mkdir com.senacor.hackingdays~bomberman2-matchmaker~1.0.0-final
+in the created directory add module.link with following content
+<path_to_project_dir>/bomberman2-mustache-renderer
+
+bin/vertx runMod com.senacor.hackingdays~bomberman2-mustache-renderer-module~1.0.0-final
---
+++ bomberman2-matchmaker/src/main/java/com/senacor/bomberman/matchmaker/MatchMakerVerticle.java
@@ -0,0 +1,58 @@
+package com.senacor.bomberman.matchmaker;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Verticle;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jchrist on 22.03.14.
+ */
+public class MatchMakerVerticle extends Verticle {
+
+    public static final String MATCHMAKER_PLAYER_REGISTER = "matchmaker.player.register";
+    public static final String MATCHMAKER_PLAYER_UNREGISTER = "matchmaker.player.unregister";
+    public static final String MATCHMAKER_PLAYER_LIST = "matchmaker.player.list";
+
+    /**
+     * String = Name of Player
+     */
+    private Set<String> playerSet = new HashSet<>();
+
+    @Override
+    public void start() {
+
+        vertx.eventBus().registerHandler(MATCHMAKER_PLAYER_REGISTER, new Handler<Message<JsonObject>>() {
+            @Override
+            public void handle(Message<JsonObject> event) {
+                String name = event.body().getString("name");
+                boolean successfullyAdded = playerSet.add(name);
+                event.reply(successfullyAdded);
+            }
+        });
+
+        vertx.eventBus().registerHandler(MATCHMAKER_PLAYER_UNREGISTER, new Handler<Message<JsonObject>>() {
+            @Override
+            public void handle(Message<JsonObject> event) {
+                String name = event.body().getString("name");
+                boolean successfullyRemoved = playerSet.remove(name);
+                event.reply(successfullyRemoved);
+            }
+        });
+
+
+        vertx.eventBus().registerHandler(MATCHMAKER_PLAYER_LIST, new Handler<Message<JsonObject>>() {
+            @Override
+            public void handle(Message<JsonObject> event) {
+                String[] playerArray = playerSet.toArray(new String[0]);
+                event.reply(new JsonArray(playerArray));
+            }
+        });
+
+
+    }
+}
