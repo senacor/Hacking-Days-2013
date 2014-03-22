d7f2b0f611f2c446caaef27ecd7bfd1e11c7071e
Michael Menzel <michael.menzel@senacor.com>
2014-03-22 02:39:20 +0100

    Umbenennung ins Englische

--- bomberman-server/src/main/java/com/senacor/bm/model/PlacedBomb.java
+++ bomberman-server/src/main/java/com/senacor/bm/model/PlacedBomb.java
@@ -3,7 +3,7 @@ package com.senacor.bm.model;
 /**
  * Created by mmenzel on 21.03.2014.
  */
-public class PlatzierteBombe {
+public class PlacedBomb {
     private Position position;
     private String spielerId;
     private int reichweite;
--- bomberman-server/src/main/java/com/senacor/bm/model/PlacedItem.java
+++ bomberman-server/src/main/java/com/senacor/bm/model/PlacedItem.java
@@ -3,12 +3,12 @@ package com.senacor.bm.model;
 /**
  * Created by mmenzel on 21.03.2014.
  */
-public class PlatziertesItem {
+public class PlacedItem {
     private Position position;
 
     private Itemtyp itemtyp;
 
-    public PlatziertesItem(Position position, Itemtyp itemtyp) {
+    public PlacedItem(Position position, Itemtyp itemtyp) {
         this.position = position;
         this.itemtyp = itemtyp;
     }
