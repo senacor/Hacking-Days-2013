0584f0dd504154b829d8e40fcf84b7247c2dd182
Andri Bremm <code@andribremm.de>
2014-03-23 17:10:50 +0100

    moved gameCounter to GameWorldVerticle

--- bomberman2-gamestate/src/main/groovy/com/senacor/hackingdays/bomberman2/gamestate/GameStarterVerticle.groovy
+++ bomberman2-gamestate/src/main/groovy/com/senacor/hackingdays/bomberman2/gamestate/GameStarterVerticle.groovy
@@ -9,24 +9,24 @@ import org.vertx.groovy.platform.Verticle
 class GameStarterVerticle extends Verticle{
 
     def gameIdToVerticleMap = [:];
-    Integer gameIdCounter = 0;
 
     @Override
     Object start() {
-        container.logger.info("started GameStarterVerticle");
-
+        // game.start -
         vertx.eventBus.registerHandler("game.start", { message ->
-            gameIdCounter++
-            //println(LockStepVerticle.class.getCanonicalName())
+            container.logger.info("HANDLER: game.start");
+
+            final Integer gameId = message.body["gameId"];
+
             println message.body
             Verticle deployable = container.deployVerticle("groovy:src/main/groovy/com/senacor/hackingdays/bomberman2/gamestate/LockStepVerticle",
-                    ["participants" : message.body["participants"], "gameId" : gameIdCounter], {doneHandler ->
+                    ["participants" : message.body["participants"], "gameId" : gameId], {doneHandler ->
                             message.body["participants"].each {
-                                vertx.eventBus.send(it.get("name")+".start", gameIdCounter)
+                                vertx.eventBus.send(it.get("name")+".start", gameId)
                             }
-                            vertx.eventBus.send("dashboard.start", gameIdCounter)
+                            vertx.eventBus.send("dashboard.start", gameId)
                         })
-            gameIdToVerticleMap.put(gameIdCounter, deployable)
+            gameIdToVerticleMap.put(gameId, deployable)
         });
 
         vertx.eventBus.registerHandler("game.stop", { message ->
